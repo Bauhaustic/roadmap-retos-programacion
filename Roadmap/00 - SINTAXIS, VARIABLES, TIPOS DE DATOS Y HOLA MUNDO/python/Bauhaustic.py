4d0a05d052e991dc2942ee1b9801977c7f072c01
Bauhaustic <venriquemc@outlook.com>
2024-07-24 18:45:44 -0600

    Create Bauhaustic.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Bauhaustic.py
@@ -0,0 +1,23 @@
+# https://www.python.org
+
+"""
+esto también
+es un comentario
+"""
+
+'''
+otro tipo de 
+comentario
+'''
+
+my_variable = "Mi variable"
+my_fariable = "Nuevo valor de mi variable"
+
+MY CONSTANT = "Mi constante" # por convención
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_oter
